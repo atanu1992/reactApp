47267020b95b48676f11bb0a2b12b7c4ab48c14b
atanu <atanusam1992@gmail.com>
2019-12-26 18:55:35 +0530

    new changes

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+// import InputField from '../shared/InputField';
 export default class Login extends Component {
 
     state = {
---
+++ src/components/shared/InputField.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+// export default function InputField(props) {
+//     state = props;
+//     state = {
+//         type    :props.type,
+//         class   :props.class,
+//         name    :props.name,
+//         id      :props.id,
+//         value   :props.value
+//     }
+//     return (
+//         <React.Fragment>
+//             <input 
+//                 type={state.type}
+//                 className={state.class} 
+//                 name={state.name} 
+//                 id={state.id} 
+//                 required="" 
+//                 value={ state.value }
+//             />
+//         </React.Fragment>
+//     )
+// }
