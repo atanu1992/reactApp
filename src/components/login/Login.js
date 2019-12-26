542259a2fc1e19fadb6d31d730343a6336910004
atanu <atanusam1992@gmail.com>
2019-12-26 10:00:07 +0530

    login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react'
 
 export default class Login extends Component {
+
+    state = {
+        username : '',
+        password : ''
+    }
+
+    changeState = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        });
+    }
+    
     render() {
+
+        const { username, password } = this.state;
         return (
             <React.Fragment>
                 <div className="row">
@@ -10,20 +24,39 @@ export default class Login extends Component {
 
                         <div className="card card-outline-secondary">
                             <div className="card-header">
-                                <h3 className="mb-0 text-center">Login</h3>
+                                <h4 className="mb-0 text-center">Login</h4>
                             </div>
                             <div className="card-body">
                                 <form className="form" autoComplete="off" id="loginForm" noValidate="" method="POST">
                                     <div className="form-group">
                                         <label htmlFor="username">Username</label>
-                                        <input type="text" className="form-control" name="username" id="username" required="" />
-                                        <div className="invalid-feedback">Please enter your username or email</div>
+                                        <input 
+                                            type="text" 
+                                            className="form-control" 
+                                            name="username" id="username" 
+                                            required="" 
+                                            value={ username } 
+                                            onChange={this.changeState}
+                                        />
+                                        <div className="invalid-feedback">
+                                            Please enter your username or email
+                                        </div>
                                     </div>
 
                                     <div className="form-group">
                                             <label>Password</label>
-                                            <input type="password" className="form-control" name="password" id="password" required=""/>
-                                                <div className="invalid-feedback">Please enter a password</div>
+                                            <input 
+                                                type="password" 
+                                                className="form-control" 
+                                                name="password" 
+                                                id="password" 
+                                                required=""
+                                                value={ password }
+                                                onChange={this.changeState}
+                                            />
+                                            <div className="invalid-feedback">
+                                                Please enter a password
+                                            </div>
                                     </div>
                                     <button type="submit" className="btn btn-success btn-md float-right" id="btnLogin">Login</button>
                                 </form>
